f23bdebb5a5687708595f90679143524f51ebd10
LSchool-Info-Tech <>
2021-04-30 06:17:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,164 @@
+
+
+##Recebendo os 2 valores corretamente e não existindo outra comida com o
+##mesmo nome na lista, a função deve adicionar a comida no dicionário
+##(food_list).#
+
+
+def add_food(nome_comida='', descricao_comida=''):
+  if type(nome_comida) is not str or type(descricao_comida) is not str:
+    print("Cara, ambos valores devem ser string")
+
+  elif nome_comida == '' or descricao_comida == '':
+    print("Vc precisa passar os 2 valores")
+
+  elif nome_comida in food_list:
+    print("comida ja cadastrada")
+
+  else:
+    food_list["nome_comida"] = descricao_comida
+    print("comida cadastrada!")
+
+
+
+
+#####!! NÃO EDITE O CÓDIGO ABAIXO DESTA LINHA !!#####
+
+#criação da food_list
+food_list = {
+  'paçoquinha': 'Um doce de amendoím brasileiro',
+  'brigadeiro': 'Um doce muito delicioso',
+  'pizza': 'um tipo de comida da italia',
+  'hamburguer': 'comida dos USA'
+}
+
+#####################################
+######## TESTES ADD_FOOD #########
+#####################################
+
+# ADD_FOOD - TESTE 1
+print("\n#### ADD_FOOD - TESTE 1")
+print("Usando add_food com valores sendo int")
+print("add_food(100, 23)\n")
+#excuta:
+add_food(100, 23)
+
+# ADD_FOOD - TESTE 2
+print("\n#### ADD_FOOD - TESTE 2")
+print("Usando add_food sem passar a descrição.")
+print("add_food('pizza')\n")
+#excuta:
+add_food('pizza')
+
+# ADD_FOOD - TESTE 3
+print("\n#### ADD_FOOD - TESTE 3")
+print("Usando add_food com comida já existente.")
+print("add_food('brigadeiro', 'Um doce brasileiro')\n")
+#excuta:
+add_food('brigadeiro', 'Um doce brasileiro')
+
+# ADD_FOOD - TESTE 4
+print("\n#### ADD_FOOD - TESTE 4")
+print("Usando add_food adicionando uma comida.")
+print("add_food('lasanha', 'Camadas de massa e molho')\n")
+#excuta:
+add_food('lasanha', 'Camadas de massa e molho')
+
+
+#####################################
+######## TESTES DELETE_FOOD #########
+#####################################
+
+# DELETE_FOOD - TESTE 1
+print("\n#### DELETE_FOOD - TESTE 1")
+print("Usando delete_food com valor sendo int")
+print("delete_food(100)\n")
+#excuta:
+delete_food(100)
+
+# DELETE_FOOD - TESTE 2
+print("\n#### DELETE_FOOD - TESTE 2")
+print("Usando delete_food sem nenhum valor.")
+print("delete_food()\n")
+#excuta:
+delete_food()
+
+# DELETE_FOOD - TESTE 3
+print("\n#### DELETE_FOOD - TESTE 3")
+print("Usando delete_food com comida que não existe na lista.")
+print("delete_food('massa')\n")
+#excuta:
+delete_food('massa')
+
+# DELETE_FOOD - TESTE 4
+print("\n#### DELETE_FOOD - TESTE 4")
+print("Usando delete_food removendo uma comida.")
+print("delete_food('paçoquinha')\n")
+#excuta:
+delete_food('paçoquinha')
+
+#####################################
+## TESTES UPDATE_FOOD ##
+#####################################
+
+# UPDATE_FOOD - TESTE 1
+print("\n#### UPDATE_FOOD - TESTE 1")
+print("Usando update_food com valores sendo int")
+print("update_food(100, 23)\n")
+#excuta:
+update_food(100, 23)
+
+# UPDATE_FOOD - TESTE 2
+print("\n#### UPDATE_FOOD - TESTE 2")
+print("Usando update_food sem passar a descrição.")
+print("update_food('pizza')\n")
+#excuta:
+update_food('pizza')
+
+# UPDATE_FOOD - TESTE 3
+print("\n#### UPDATE_FOOD - TESTE 3")
+print("Usando update_food com comida não existente.")
+print("update_food('sorvete', 'Um doce gelado da italia')\n")
+#excuta:
+update_food('sorvete', 'Um doce gelado da italia')
+
+
+# UPDATE_FOOD - TESTE 4
+print("\n#### UPDATE_FOOD - TESTE 4")
+print("Usando update_food e atualizando uma comida.")
+print("update_food('brigadeiro', 'Melhor doce do mundo.')\n")
+#excuta:
+update_food('brigadeiro', 'Melhor doce do mundo.')
+
+#####################################
+## TESTES GET_FOOD ##
+#####################################
+
+# GET_FOOD - TESTE 1
+print("\n#### GET_FOOD - TESTE 1")
+print("Usando get_food com valor sendo int")
+print("get_food(505)\n")
+#excuta:
+get_food(505)
+
+# GET_FOOD - TESTE 2
+print("\n#### GET_FOOD - TESTE 2")
+print("Usando get_food sem passar a comida.")
+print("get_food()\n")
+
+get_food()
+
+# GET_FOOD - TESTE 3
+print("\n#### GET_FOOD - TESTE 3")
+print("Usando get_food com comida não existente.")
+print("get_food('noodle')\n")
+get_food('noodle')
+
+# GET_FOOD - TESTE 4
+print("\n#### GET_FOOD - TESTE 4")
+print("Usando get_food e pesquisando a descrição uma comida.")
+print("get_food('hamburguer')\n")
+get_food('hamburguer')
+
+
+#####!!!!!!!!!!!!!! THE END !!!!!!!!!!!!!!#####
